257145d03c2fd6f4bfa994f11b302214128b0644
Michael Kelly <michaelkelly322@gmail.com>
2014-03-19 02:22:12 -0400

    created initial skelaton code for pdf action

--- app/controllers/pdf_service_controller.rb
+++ app/controllers/pdf_service_controller.rb
@@ -1,4 +1,50 @@
 class PdfServiceController < ApplicationController
   def convert
+    @error = false
+    unless params[:work].nil?
+      # => validate title
+      unless params[:work][:title].nil?
+        work[:title] = params[:work][:title]
+      else
+        render_error(422)
+      end
+      
+      # => Validate author
+      unless params[:work][:author].nil?
+        work[:author] = params[:work][:author]
+      else
+        render_error(422)
+      end
+      
+      #validate body
+      unless params[:work][:body].nil?
+        work[:body] = params[:work][:body]
+      else
+        render_error(422)
+      end
+      
+      #validate callback url
+      unless params[:work][:callback].nil?
+        work[:callback] = params[:work][:callback]
+      else
+        render_error(422)
+      end
+      
+      unless @error
+        # => return ok status
+        # => begin pdf generation
+        # => post pdf to call-back url
+      else
+        render_error(500)
+      end
+    else
+      render_error(422)
+    end
   end
+  
+  private
+    def render_error(status=500)
+      @error = true
+      render(:file => File.join(Rails.root, 'public/'.join(status.to_s,'.html')), :status => status, :layout => false)
+    end
 end
